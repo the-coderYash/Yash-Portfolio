dca34c62652438df4381fdca44efb4ee4a5961ae
Yash Raj <yashraj00112@gmail.com>
2024-05-17 18:15:13 +0530

    //Configuring Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,14 +1,19 @@
-import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/hero-img.png';
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
 
 function Hero() {
-          return (
-          <section id="hero">
-          <div>
-          <img src={heroImg} alt="" />
-          </div>
-          </section>
-          );
+  return (
+    <section id="hero">
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="Animated Profile Picture of Yash Raj"
+        />
+        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" />
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
