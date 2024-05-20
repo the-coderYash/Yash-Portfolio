a25a6c1808cd814ba4cc27084b0a75bb10abb65a
Yash Raj <yashraj00112@gmail.com>
2024-05-20 16:49:31 +0530

    //Adding dark background feature

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
-import twitterIcon from '../../assets/twitter-light.svg';
-import githubIcon from '../../assets/github-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
 import CV from '../../assets/cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
@@ -13,6 +17,9 @@ function Hero() {
   const { theme, toggleTheme } = useTheme(); //
 
   const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
   return (
     <section id='hero' className={styles.container}>
