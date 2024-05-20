ef55d6ddaa597349685d1a4dfb78a5454c0f982b
Yash Raj <yashraj00112@gmail.com>
2024-05-21 00:51:42 +0530

    //Working in Projects.jsx

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectsStyles.module.css';
+
+function Projects() {
+  return (
+    <section id='projects' className={styles.container}>
+      <h1 className='sectionTitle'>Projects</h1>
+    </section>
+  );
+}
+
+export default Projects;
