2739c4096b4e9cf668a429615aae8c8b15a9c9cd
Yash Raj <yashraj00112@gmail.com>
2024-05-21 12:26:11 +0530

    //Adding Project Section with Icons

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,9 +1,20 @@
 import styles from './ProjectsStyles.module.css';
+import GCEWEB from '../../assets/gceweb.png';
 
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
+      <div className={styles.projectsContainer}>
+        <a
+          href='https://github.com/the-coderYash/GCE-Website-Project'
+          target='_blank'
+        >
+          <img className='hover' src={GCEWEB} alt='GCE Website' />
+          <h3>Gaya College Of ENGG.</h3>
+          <p>Website for college</p>
+        </a>
+      </div>
     </section>
   );
 }
