c2bd5013c342653ced40bb807f7aa961bdf01275
Yash Raj <yashraj00112@gmail.com>
2024-05-21 13:14:52 +0530

    //Adding Projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,19 +1,26 @@
+/* eslint-disable no-unused-vars */
 import styles from './ProjectsStyles.module.css';
 import GCEWEB from '../../assets/gceweb.png';
+import clock from '../../assets/clock.png';
+import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className={styles.projectsContainer}>
-        <a
-          href='https://github.com/the-coderYash/GCE-Website-Project'
-          target='_blank'
-        >
-          <img className='hover' src={GCEWEB} alt='GCE Website' />
-          <h3>Gaya College Of ENGG.</h3>
-          <p>Website for college</p>
-        </a>
+        <ProjectCard
+          src={GCEWEB}
+          link='https://github.com/the-coderYash/GCE-Website-Project'
+          h3='Gaya College Of ENGG.'
+          p='Website for college'
+        />
+        <ProjectCard
+          src={clock}
+          link='https://github.com/the-coderYash/Analog-Clock'
+          h3='Analog Clock'
+          p='A Beautiful Analog Clock'
+        />
       </div>
     </section>
   );
