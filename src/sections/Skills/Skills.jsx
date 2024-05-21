087c4ff201207f73af22fab2f48a691253a59bad
Yash Raj <yashraj00112@gmail.com>
2024-05-22 01:07:17 +0530

    //Added Skills Section

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -6,10 +6,33 @@ import SkillList from '../../common/SkillList';
 function Skills() {
   return (
     <section id='skills' className={styles.container}>
-      <h1>Skills</h1>
+      <h1 className='sectionTitle'>Skills</h1>
       <div className={styles.skillList}>
-        <SkillList />
+        <SkillList src={checkMarkIcon} skill='HTML' />
+        <SkillList src={checkMarkIcon} skill='CSS' />
+        <SkillList src={checkMarkIcon} skill='JavaScript' />
+        <SkillList src={checkMarkIcon} skill='TypeScript' />
+        <SkillList src={checkMarkIcon} skill='React' />
       </div>
+      <hr />
+
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill='Angular' />
+        <SkillList src={checkMarkIcon} skill='Tailwind CSS' />
+        <SkillList src={checkMarkIcon} skill='Python' />
+        <SkillList src={checkMarkIcon} skill='Java' />
+        <SkillList src={checkMarkIcon} skill='PHP' />
+      </div>
+      <hr />
+
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill='Redux' />
+        <SkillList src={checkMarkIcon} skill='Jest' />
+        <SkillList src={checkMarkIcon} skill='WebPack' />
+        <SkillList src={checkMarkIcon} skill='Git' />
+        <SkillList src={checkMarkIcon} skill='Bootstrap' />
+      </div>
+      <hr />
     </section>
   );
 }
