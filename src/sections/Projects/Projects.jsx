9afbf7d145a0b321e9166f33d974ce3a59fc3b60
Yash Raj <yashraj00112@gmail.com>
2024-05-21 14:48:43 +0530

    //Added More Projects from GitHub

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -5,6 +5,8 @@ import clock from '../../assets/clock.png';
 import calculator from '../../assets/calculator.png';
 import loginPage from '../../assets/loginPage.png';
 import jokesWebsite from '../../assets/jokes.png';
+import keyCodes from '../../assets/keyCodes.png';
+import soundBoard from '../../assets/soundBoard.png';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -26,21 +28,33 @@ function Projects() {
         />
         <ProjectCard
           src={calculator}
-          link=''
+          link='https://github.com/the-coderYash/CodSoft-Calculator'
           h3='Neumorphic Calculator '
           p='Calculator with Neumorphism Design '
         />
         <ProjectCard
           src={loginPage}
-          link=''
+          link='https://github.com/the-coderYash/Project-8---Login-Page'
           h3='Login Page'
           p='Login Page with animation'
         />
         <ProjectCard
           src={jokesWebsite}
-          link=''
+          link='https://github.com/the-coderYash/Project-10---Jokes-Website'
           h3='Online Jokes Generator'
           p='Website which generate Jokes'
+        />
+        <ProjectCard
+          src={soundBoard}
+          link='https://github.com/the-coderYash/Project-9---Sound-Board'
+          h3='Sound Board'
+          p='Can play sound'
+        />
+        <ProjectCard
+          src={keyCodes}
+          link='https://github.com/the-coderYash/Project-11---Key-Codes'
+          h3='Key Codes'
+          p='Know your Key Codes'
         />
       </div>
     </section>
